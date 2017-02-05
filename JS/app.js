67a04719df28e9223ab388c61c7e6016433868db
Zak <zakattack26111@gmail.com>
2017-02-04 16:01:29 -1000

    fixed code spacing

--- JS/app.js
+++ JS/app.js
@@ -75,114 +75,114 @@ var totalPoints = 0;
 
 function cardCheck(x){
   if(x == 1){
-  	firstFlip = 1;
-  	flipCounter++;
-  	document.getElementById("a").style.transform = "rotateY(0deg)";
-  	document.getElementById("b").style.transform = "rotateY(-180deg)"; 
+    firstFlip = 1;
+    flipCounter++;
+    document.getElementById("a").style.transform = "rotateY(0deg)";
+    document.getElementById("b").style.transform = "rotateY(-180deg)"; 
   }
 
   if(x == 2){
-  	secondFlip = 1;
-  	flipCounter++;
-  	document.getElementById("c").style.transform = "rotateY(0deg)";
-  	document.getElementById("d").style.transform = "rotateY(-180deg)";
+    secondFlip = 1;
+    flipCounter++;
+    document.getElementById("c").style.transform = "rotateY(0deg)";
+    document.getElementById("d").style.transform = "rotateY(-180deg)";
   }
 
   if(x == 3){
-  	firstFlip = 2;
-  	flipCounter++;
-  	document.getElementById("e").style.transform = "rotateY(0deg)";
-  	document.getElementById("f").style.transform = "rotateY(-180deg)";
+    firstFlip = 2;
+    flipCounter++;
+    document.getElementById("e").style.transform = "rotateY(0deg)";
+    document.getElementById("f").style.transform = "rotateY(-180deg)";
   }
 
   if(x == 4){
-  	secondFlip = 2;
-  	flipCounter++;
-  	document.getElementById("g").style.transform = "rotateY(0deg)";
-  	document.getElementById("h").style.transform = "rotateY(-180deg)";
+    secondFlip = 2;
+    flipCounter++;
+    document.getElementById("g").style.transform = "rotateY(0deg)";
+    document.getElementById("h").style.transform = "rotateY(-180deg)";
   }
 
   if(x == 5){
-  	firstFlip = 3;
-  	flipCounter++;
-  	document.getElementById("i").style.transform = "rotateY(0deg)";
-  	document.getElementById("j").style.transform = "rotateY(-180deg)";
+    firstFlip = 3;
+    flipCounter++;
+    document.getElementById("i").style.transform = "rotateY(0deg)";
+    document.getElementById("j").style.transform = "rotateY(-180deg)";
   }
   
   if(x == 6){
-  	secondFlip = 3;
-  	flipCounter++;
-  	document.getElementById("k").style.transform = "rotateY(0deg)";
-  	document.getElementById("l").style.transform = "rotateY(-180deg)";
+    secondFlip = 3;
+    flipCounter++;
+    document.getElementById("k").style.transform = "rotateY(0deg)";
+    document.getElementById("l").style.transform = "rotateY(-180deg)";
   }
   
   if(x == 7){
-  	firstFlip = 4;
-  	flipCounter++;
-  	document.getElementById("m").style.transform = "rotateY(0deg)";
-  	document.getElementById("n").style.transform = "rotateY(-180deg)";
+    firstFlip = 4;
+    flipCounter++;
+    document.getElementById("m").style.transform = "rotateY(0deg)";
+    document.getElementById("n").style.transform = "rotateY(-180deg)";
   }
   
   if(x == 8){
-  	secondFlip = 4;
-  	flipCounter++;
-  	document.getElementById("o").style.transform = "rotateY(0deg)";
-  	document.getElementById("p").style.transform = "rotateY(-180deg)";
+    secondFlip = 4;
+    flipCounter++;
+    document.getElementById("o").style.transform = "rotateY(0deg)";
+    document.getElementById("p").style.transform = "rotateY(-180deg)";
   }
   
   if(firstFlip == secondFlip){
-  	var points = document.getElementById("points");
-  	console.log("match");
+    var points = document.getElementById("points");
+    console.log("match");
     if(firstFlip == 1){
-    	totalPoints++
-    	var ab = document.getElementById("one");
+      totalPoints++
+      var ab = document.getElementById("one");
       ab.style.animationPlayState = "running";
       var cd = document.getElementById("two");
       cd.style.animationPlayState = "running";
       points.innerText = "Points: " + totalPoints;
       /*setTimeout(
-  			function noneOne(){
-  				ab.style.display = "none"
-  				cd.style.display = "none"
+  	function noneOne(){
+  	  ab.style.display = "none"
+  	  cd.style.display = "none"
         }, 1500)*/
     }
     if(firstFlip == 2){
-    	totalPoints++
-    	var ef = document.getElementById("three");
+      totalPoints++
+      var ef = document.getElementById("three");
       ef.style.animationPlayState = "running";
       var gh = document.getElementById("four");
       gh.style.animationPlayState = "running";
       points.innerText = "Points: " + totalPoints;
       /*setTimeout(
-  			function noneTwo(){
-  				ef.style.display = "none"
-  				gh.style.display = "none"
+  	function noneTwo(){
+  	  ef.style.display = "none"
+  	  gh.style.display = "none"
         }, 1500)*/
     }
     if(firstFlip == 3){
-    	totalPoints++
-    	var ij = document.getElementById("five");
+      totalPoints++
+      var ij = document.getElementById("five");
       ij.style.animationPlayState = "running";
       var kl = document.getElementById("six");
       kl.style.animationPlayState = "running";
       points.innerText = "Points: " + totalPoints;
       /*setTimeout(
-  			function noneThree(){
-  				ij.style.display = "none"
-  				kl.style.display = "none"
+	function noneThree(){
+	  ij.style.display = "none"
+	  kl.style.display = "none"
         }, 1500)*/
     }
     if(firstFlip == 4){
-    	totalPoints++
-    	var mn = document.getElementById("seven");
+      totalPoints++
+      var mn = document.getElementById("seven");
       mn.style.animationPlayState = "running";
       var op = document.getElementById("eight");
       op.style.animationPlayState = "running";
       points.innerText = "Points: " + totalPoints;
       /*setTimeout(
-  			function noneFour(){
-  				mn.style.display = "none"
-  				op.style.display = "none"
+  	function noneFour(){
+  	  mn.style.display = "none"
+  	  op.style.display = "none"
         }, 1500)*/
     }
   	flipCounter = 0;
@@ -195,23 +195,23 @@ function cardCheck(x){
   
   //delay timing of card flipping back over
   setTimeout(
-  	function delay(){
-	  	if(flipCounter == 2 && firstFlip != secondFlip){
-		    console.log("no match");
-		    flipCounter = 0;
-		    firstFlip = 0;
-		  	secondFlip = 0;
-        
-		  	var idFrontNames = ["a", "c", "e", "g", "i", "k", "m", "o"];
-  			var idBackNames = ["b", "d", "f", "h", "j", "l", "n", "p"];
-				for(var z = idFrontNames.length; z > 0; z--){
-					frontId = idFrontNames.splice(0, 1)[0];
-					backId = idBackNames.splice(0, 1)[0];
-          
-					document.getElementById(frontId).style.transform = "rotateY(-180deg)";
-		  	  document.getElementById(backId).style.transform = "rotateY(0deg)";		
-				}
-	    }
+    function delay(){
+      if(flipCounter == 2 && firstFlip != secondFlip){
+        console.log("no match");
+	flipCounter = 0;
+	firstFlip = 0;
+        secondFlip = 0;
+
+	var idFrontNames = ["a", "c", "e", "g", "i", "k", "m", "o"];
+	var idBackNames = ["b", "d", "f", "h", "j", "l", "n", "p"];
+	for(var z = idFrontNames.length; z > 0; z--){
+	  frontId = idFrontNames.splice(0, 1)[0];
+	  backId = idBackNames.splice(0, 1)[0];
+
+	  document.getElementById(frontId).style.transform = "rotateY(-180deg)";
+	  document.getElementById(backId).style.transform = "rotateY(0deg)";		
+	}
+      }
     }, 1500);
 
   //winner ending animation
